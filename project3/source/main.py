6de04c404e2739bd181fe4fbc34f689d82b47d44
anthemwingate <anthemwingate@users.noreply.github.com>
2019-07-04 10:36:58 -0400

    next test rev

--- project3/source/main.py
+++ project3/source/main.py
@@ -2,6 +2,7 @@ import unittest
 from gedcom_file_parser import print_pretty_table
 import util_date
 import US07_US08_Test_Suite
+import Unit_test_us11_us12
 import Sprint2_TestSuite
 
 if __name__ == '__main__':
@@ -10,6 +11,8 @@ if __name__ == '__main__':
     suit1 = unittest.TestLoader().loadTestsFromModule(util_date)
     suit2 = unittest.TestLoader().loadTestsFromModule(Sprint2_TestSuite)
     suit3 = unittest.TestLoader().loadTestsFromModule(US07_US08_Test_Suite)
+    suit4 = unittest.TestLoader().loadTestsFromModule(Unit_test_us11_us12)
     unittest.TextTestRunner(verbosity=2).run(suit1)
     unittest.TextTestRunner(verbosity=2).run(suit2)
     unittest.TextTestRunner(verbosity=2).run(suit3)
+    unittest.TextTestRunner(verbosity=2).run(suit4)
