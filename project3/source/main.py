d528858489f72262f20372ab72cc71b883d41abf
anthemwingate <anthemwingate@users.noreply.github.com>
2019-07-04 10:33:37 -0400

    next rev

--- project3/source/main.py
+++ project3/source/main.py
@@ -2,13 +2,13 @@ import unittest
 from gedcom_file_parser import print_pretty_table
 import util_date
 import US07_US08_Test_Suite
-import unit_test_us1_us2_us3_us4_us5_us6_us10
+import Sprint2_TestSuite
 
 if __name__ == '__main__':
     
-    print_pretty_table("C:/Users/Anthe/OneDrive/Documents/Stevens/SSW 555/GEDCOM/Projects/Sprint2/faultyDates.ged")
+    print_pretty_table("C:/Users/Anthe/OneDrive/Documents/Stevens/SSW 555/GEDCOM/Projects/Sprint2/Sprint2test.ged")
     suit1 = unittest.TestLoader().loadTestsFromModule(util_date)
-    suit2 = unittest.TestLoader().loadTestsFromModule(unit_test_us1_us2_us3_us4_us5_us6_us10)
+    suit2 = unittest.TestLoader().loadTestsFromModule(Sprint2_TestSuite)
     suit3 = unittest.TestLoader().loadTestsFromModule(US07_US08_Test_Suite)
     unittest.TextTestRunner(verbosity=2).run(suit1)
     unittest.TextTestRunner(verbosity=2).run(suit2)
