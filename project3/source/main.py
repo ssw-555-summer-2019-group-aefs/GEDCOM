fe6c53b6abb4a40006bceeb1d36fa3c8bea054cf
Saransh Ahlawat <Saransh.Ahlawat@viacomcontractor.com>
2019-07-04 18:27:37 -0400

    .

--- project3/source/main.py
+++ project3/source/main.py
@@ -1,5 +1,6 @@
 import unittest
-import os 
+import sys
+import os
 
 from gedcom_file_parser import print_pretty_table
 import util_date
@@ -8,13 +9,13 @@ import unit_test_us1_us2_us3_us4_us5_us6_us10
 
 if __name__ == '__main__':
     
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    print_pretty_table(f"{dir_path}/project3/data/test.ged")
-    suit1 = unittest.TestLoader().loadTestsFromModule(util_date)
-    suit2 = unittest.TestLoader().loadTestsFromModule(unit_test_us1_us2_us3_us4_us5_us6_us10)
-    suit3 = unittest.TestLoader().loadTestsFromModule(US07_US08_Test_Suite)
-    unittest.TextTestRunner(verbosity=2).run(suit1)
-    unittest.TextTestRunner(verbosity=2).run(suit2)
-    unittest.TextTestRunner(verbosity=2).run(suit3)
+    dir_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+    # dir_path = os.getcwd("../");
+    print_pretty_table(f"{dir_path}/data/project01.ged")
+    # suit1 = unittest.TestLoader().loadTestsFromModule(util_date)
+    # suit2 = unittest.TestLoader().loadTestsFromModule(unit_test_us1_us2_us3_us4_us5_us6_us10)
+    # suit3 = unittest.TestLoader().loadTestsFromModule(US07_US08_Test_Suite)
+    # unittest.TextTestRunner(verbosity=2).run(suit1)
+    # unittest.TextTestRunner(verbosity=2).run(suit2)
+    # unittest.TextTestRunner(verbosity=2).run(suit3)
 
