45296ae1e658d3051e25aea3d85ced960e4870ce
anthemwingate <anthemwingate@users.noreply.github.com>
2019-07-05 16:05:54 -0400

    US17_US18 test File

--- project3/source/gedcom_file_parser.py
+++ project3/source/gedcom_file_parser.py
@@ -3,10 +3,10 @@ from utils import LEVEL_TAGS, get_families_pretty_table_order, get_family_info_t
 from util_date import Date
 from Sprint1 import get_spouse_block
 from Sprint2 import get_child_block
-from Sprint3 import get_recent_block
+#from Sprint3 import get_recent_block
 from US07_US08 import check_150_years_age, check_birth_before_marriage_of_parents
 from US09 import birth_before_parents_death
-from US11_US12 import check_bigamy, check_parents_not_too_old
+from us11_us12 import check_bigamy, check_parents_not_too_old
 
 
 def gedcom_file_parser(path):
