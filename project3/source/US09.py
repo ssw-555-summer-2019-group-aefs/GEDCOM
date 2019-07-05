d47f6fcd79e2fe01af592b982168f52e4ce51d6d
Saransh Ahlawat <Saransh.Ahlawat@viacomcontractor.com>
2019-07-05 16:54:36 -0400

    User story 9 refactor

--- project3/source/US09.py
+++ project3/source/US09.py
@@ -7,13 +7,14 @@ def birth_before_parents_death(individuals_dict, families_dict):
         if husb_info.get('DEAT') != None and husb_info.get('DEAT') != 'NA':
             if family_info.get('CHIL') != None and family_info.get('CHIL') != 'NA':
                 for child in family_info.get('CHIL'):
+
                     child_info = individuals_dict[child]
                     if husb_info.get('DEAT') == "NA" or type(husb_info.get('DEAT').date_time_obj) is str or child_info.get('BIRT') == "NA" or type(child_info.get('BIRT')) is str:
                         pass
                     else:
                         try:
                             if Date.get_dates_difference(husb_info.get('DEAT').date_time_obj, child_info.get('BIRT').date_time_obj) < 0:
-                                raise ValueError(f"ERROR: Husband died before the birth of his child - {family_id}")
+                                raise ValueError(f"ERROR: US09: {family_id}: Husband died before the birth of his child")
                         except ValueError as e:
                             print(e)
 
@@ -26,6 +27,6 @@ def birth_before_parents_death(individuals_dict, families_dict):
                     else:
                         try:
                             if Date.get_dates_difference(wife_info.get('DEAT').date_time_obj, child_info.get('BIRT').date_time_obj) < 0:
-                                raise ValueError(f"ERROR: Wife died before the birth of her child - {family_id}")
+                                raise ValueError(f"ERROR: US09: {family_id} Wife died before the birth of her child")
                         except ValueError as e:
                             print(e)
