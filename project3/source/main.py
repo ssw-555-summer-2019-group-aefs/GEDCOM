a39192eaab7c1ac2d98949358057e51aa4f8279b
anthemwingate <anthemwingate@users.noreply.github.com>
2019-07-06 00:50:27 -0400

    un-comment in main

--- project3/source/main.py
+++ project3/source/main.py
@@ -8,14 +8,14 @@ import Sprint2_TestSuite
 if __name__ == '__main__':
     
     print_pretty_table("C:/Users/Anthe/OneDrive/Documents/GitHub/GEDCOM/project3/source/sprint2userstorytest.ged")
-    #suit1 = unittest.TestLoader().loadTestsFromModule(util_date)
+    suit1 = unittest.TestLoader().loadTestsFromModule(util_date)
     suit2 = unittest.TestLoader().loadTestsFromModule(Sprint2_TestSuite)
-    #suit3 = unittest.TestLoader().loadTestsFromModule(US07_US08_Test_Suite)
-    #suit4 = unittest.TestLoader().loadTestsFromModule(Unit_test_us11_us12)
+    suit3 = unittest.TestLoader().loadTestsFromModule(US07_US08_Test_Suite)
+    suit4 = unittest.TestLoader().loadTestsFromModule(Unit_test_us11_us12)
     
 
-    #unittest.TextTestRunner(verbosity=2).run(suit1)
+    unittest.TextTestRunner(verbosity=2).run(suit1)
     unittest.TextTestRunner(verbosity=2).run(suit2)
-    #unittest.TextTestRunner(verbosity=2).run(suit3)
-    #unittest.TextTestRunner(verbosity=2).run(suit4)
+    unittest.TextTestRunner(verbosity=2).run(suit3)
+    unittest.TextTestRunner(verbosity=2).run(suit4)
     
