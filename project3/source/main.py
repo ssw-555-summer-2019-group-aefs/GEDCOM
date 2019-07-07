e237f7cfa34b436de57ecac030bcfc33846e4fe1
Saransh Ahlawat <Saransh.Ahlawat@viacomcontractor.com>
2019-07-06 22:36:15 -0400

    Added sys exit status

--- project3/source/main.py
+++ project3/source/main.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import sys
 from gedcom_file_parser import print_pretty_table
 import util_date
 import US07_US08_Test_Suite
@@ -15,8 +16,17 @@ if __name__ == '__main__':
     suit3 = unittest.TestLoader().loadTestsFromModule(US07_US08_Test_Suite)
     suit4 = unittest.TestLoader().loadTestsFromModule(unit_tests_us11_us12)
     suit5 = unittest.TestLoader().loadTestsFromModule(US22_unit_test)
-    unittest.TextTestRunner(verbosity=2).run(suit1)
-    unittest.TextTestRunner(verbosity=2).run(suit2)
-    unittest.TextTestRunner(verbosity=2).run(suit3)
-    unittest.TextTestRunner(verbosity=2).run(suit4)
-    unittest.TextTestRunner(verbosity=2).run(suit5)
+
+    test_result1 = not unittest.TextTestRunner(verbosity=2).run(suit1).wasSuccessful()
+    test_result2 = not unittest.TextTestRunner(verbosity=2).run(suit2).wasSuccessful()
+    test_result3 = not unittest.TextTestRunner(verbosity=2).run(suit3).wasSuccessful()
+    test_result4 = not unittest.TextTestRunner(verbosity=2).run(suit4).wasSuccessful()
+    test_result5 = not unittest.TextTestRunner(verbosity=2).run(suit5).wasSuccessful()
+
+    print("*******:", test_result1, test_result2, test_result3, test_result4, test_result5)
+
+    final_test_result = (test_result1 and test_result2 and test_result3 and test_result4 and test_result5)
+
+    print("******* final:", final_test_result)
+
+    sys.exit(final_test_result)
