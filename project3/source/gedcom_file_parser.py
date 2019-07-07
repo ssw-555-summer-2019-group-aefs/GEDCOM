1a517012da7188de6816c3a8aca0a58190ec35a7
anthemwingate <49780762+anthemwingate@users.noreply.github.com>
2019-07-06 21:34:35 -0400

    Update gedcom_file_parser.py

--- project3/source/gedcom_file_parser.py
+++ project3/source/gedcom_file_parser.py
@@ -205,7 +205,7 @@ def print_families_pretty_table(families_dict, individuals_dict):
 
 def main():
     dir_abs_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
-    directory_path = f"{dir_abs_path}/data/ssprint2userstorytest.ged"
+    directory_path = f"{dir_abs_path}/data/sprint2userstorytest.ged"
     print_pretty_table(directory_path)
 
 
