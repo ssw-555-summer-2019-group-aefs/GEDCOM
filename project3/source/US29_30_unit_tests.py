3216aac8f497d681c41326fa6179c4a3822171cc
Erik-Bornako <ebornako@stevens.edu>
2019-07-17 21:32:25 -0400

    Changed to Test Against us29.ged and us30.ged

--- project3/source/US29_30_unit_tests.py
+++ project3/source/US29_30_unit_tests.py
@@ -12,26 +12,27 @@ class TestUserStories(unittest.TestCase):
     def test_user_stories(self):
 
         dir_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
-        directory_path = f'{dir_path}/data/sprint3userstorytest.ged'
-        individuals, families = gedcom_file_parser(directory_path)
+        directory_path_us29 = f'{dir_path}/data/us29.ged'
+        directory_path_us30 = f'{dir_path}/data/us30.ged'
+        individuals_us29, families_us29 = gedcom_file_parser(directory_path_us29)
+        individuals_us30, families_us30 = gedcom_file_parser(directory_path_us30)
 
         # Expected Result for US29
         us29_pt = PrettyTable(field_names=["ID", "Name"])
-        us29_pt.add_row(['@I1@','Tristin /Evans/'])
-        us29_pt.add_row(['@I2@','Angelo /Rose/'])
-        us29_pt.add_row(['@I4@','Phil /Evans/'])
-        us29_pt.add_row(['@I6@','Sheila /Evans/'])
-        us29_pt.add_row(['@I9@', 'Trish /Rose/'])
+        us29_pt.add_row(['@I7@','Peter /Evans/'])
+        us29_pt.add_row(['@I10@','Darren /Johnson/'])
         us29_str = "US29: List: The following individuals are deceased in the family\n" + str(us29_pt)
 
-        self.assertEqual(list_deceased(individuals), us29_str)
+        self.assertEqual(list_deceased(individuals_us29), us29_str)
        
         #US30 Expected Results
         us30_pt = PrettyTable(field_names=["Family ID", "Husband ID", "Husband Name", "Wife ID", "Wife Name"])
-        us30_pt.add_row(['@F4@', '@I17@', 'Frederick /Rose/', '@I16@', 'Cynthia /Rose/'])
+        us30_pt.add_row(['@F1@', '@I5@', 'Angelo /Rose/', '@I1@', 'Tristin /Evans/'])
+        us30_pt.add_row(['@F2@', '@I2@', 'Phil /Evans/', '@I3@', 'Sheila /Lynn/'])
+        us30_pt.add_row(['@F4@', '@I11@', 'David /Lynn/', '@I12@', 'Victoria /Taylor/'])
         us30_str = "US30: List: The following lists living married couples by family\n" + str(us30_pt)
 
-        self.assertEqual(list_living_married(individuals, families), us30_str)
+        self.assertEqual(list_living_married(individuals_us30, families_us30), us30_str)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
