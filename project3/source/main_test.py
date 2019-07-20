66aa987a8c626ae704a22449d607f94abe6d4d4d
Erik-Bornako <ebornako@stevens.edu>
2019-07-20 10:20:15 -0400

    Update main_test.py

--- project3/source/main_test.py
+++ project3/source/main_test.py
@@ -5,12 +5,12 @@ import util_date
 import US07_US08_Test_Suite
 import US22_unit_test
 import unit_tests_us11_us12
-import AWFS_TestSuite
+import Sprint2_TestSuite
 import US29_30_unit_tests
 
 if __name__ == '__main__':
     suit1 = unittest.TestLoader().loadTestsFromModule(util_date)
-    suit2 = unittest.TestLoader().loadTestsFromModule(AWFS_TestSuite)
+    suit2 = unittest.TestLoader().loadTestsFromModule(Sprint2_TestSuite)
     suit3 = unittest.TestLoader().loadTestsFromModule(US07_US08_Test_Suite)
     suit4 = unittest.TestLoader().loadTestsFromModule(unit_tests_us11_us12)
     suit5 = unittest.TestLoader().loadTestsFromModule(US22_unit_test)
